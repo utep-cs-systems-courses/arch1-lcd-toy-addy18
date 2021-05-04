e6f29ceb30dd75b644a5b3bb628e88bcd8213013
Addy <amgalvan@miners.utep.edu>
2021-05-03 18:44:13 -0600

    added methods to .h

---
+++ project/statemachine.h
@@ -0,0 +1,12 @@
+#ifndef stateMachine_included
+#define stateMachine_included
+
+char toggle_red();
+char toggle_green();
+char state1();
+char state2();
+char state3();
+char state4();
+
+#endif
+	   
